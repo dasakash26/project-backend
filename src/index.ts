f603ddb498e9256ed359f71be673e03f5952ee44
Asmit Deb <47671715+asmitdeb@users.noreply.github.com>
2024-11-21 23:23:08 +0530

    neon db prisma connection, signup route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express, {Request, Response} from "express";
+import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import "dotenv/config";
+
+const prisma = new PrismaClient();
 
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-    res.json({message: "Hello"});
+app.post("/signup", async (req: Request, res: Response) => {
+    await prisma.user.create({data: {email: req.body.email, password: req.body.password}});
+    res.status(201).json({message: "User created successfully!"});
 })
 
 app.listen(PORT, () => {
