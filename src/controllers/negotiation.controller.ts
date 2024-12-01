58f527cc5dab5a2318f81babfa640d494e626692
Asmit Deb <47671715+asmitdeb@users.noreply.github.com>
2024-12-01 20:53:01 +0530

    negotiation, counter offers

--- src/controllers/negotiation.controller.ts
+++ src/controllers/negotiation.controller.ts
@@ -31,12 +31,52 @@ export const createNegotiation = async (req: Request, res: Response) => {
 
 export const getNegotiations = async (req: Request, res: Response) => {
   try {
-    const negotiations = await prisma.negotiation.findMany();
+    // send only those negotiations where the user is either the creator or the offeredTo
+    const negotiations = await prisma.negotiation.findMany({
+      where: {
+        OR: [{ createdById: req.user.id }, { offeredToId: req.user.id }],
+      },
+    });
     const currentTerms = await prisma.currentTerms.findMany();
     console.log("negotiations", negotiations);
     res.status(200).json({ negotiations,currentTerms });
   } catch (error) {
     console.log("Error getting negotiations:", error);
     res.status(500).json({ error: "Failed to get negotiations" });
+  }
+};
+
+export const getCurrentTerms = async (req: Request, res: Response) => {
+  try {
+    const currentTerms = await prisma.currentTerms.findUnique({
+      where: { id: req.params.currentTermsId },
+    });
+    res.status(200).json({ currentTerms });
+  } catch (error) {
+    console.log("Error getting current terms:", error);
+    res.status(500).json({ error: "Failed to get current terms" });
+  }
+};
+
+export const updateCurrentTerms = async (req: Request, res: Response) => {
+  try {
+    console.log("req.body", req.body);
+    const currentTerms = await prisma.currentTerms.update({
+      where: { id: req.params.currentTermsId },
+      data: { quantity: parseInt(req.body.quantity), price: parseInt(req.body.price), harvestTime: new Date(req.body.harvestTime), location: req.body.location , paymentTerms: req.body.paymentTerms},
+    });
+    //change the turn from my turn to the other user's turn
+    const negotiation = await prisma.negotiation.findFirst({
+        where: { currentTermsId: req.params.currentTermsId }
+      }); 
+    const updatedNegotiation = await prisma.negotiation.update({
+      where: { id: negotiation!.id },
+      data: { turn: negotiation!.turn === negotiation!.offeredToId ? negotiation!.createdById : negotiation!.offeredToId },
+    });
+
+    res.status(200).json({ currentTerms, updatedNegotiation });
+  } catch (error) {
+    console.log("Error updating current terms:", error);
+    res.status(500).json({ error: "Failed to update current terms" });
   }
 };
--- src/router/negotiationRouter.ts
+++ src/router/negotiationRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createNegotiation, getNegotiations } from "../controllers/negotiation.controller";
+import { createNegotiation, getNegotiations, getCurrentTerms, updateCurrentTerms } from "../controllers/negotiation.controller";
 import requireAuth from "../middleware/checkAuth";
 
 export const negotiationRouter = Router();
 //@ts-ignore
 negotiationRouter.post("/create", requireAuth, createNegotiation);
 negotiationRouter.get("/", requireAuth, getNegotiations);
+negotiationRouter.get("/:currentTermsId", requireAuth, getCurrentTerms);
+negotiationRouter.post("/update/:currentTermsId", requireAuth, updateCurrentTerms);
