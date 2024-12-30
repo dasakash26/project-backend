629b4e2e2955b716831a6d84fd4a4724d0e1dbe7
Akash Das <akashd2664@gmail.com>
2024-12-30 17:40:59 +0530

    minor bug fix

--- prisma/migrations/20241212034245_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "CurrentTerms" ADD COLUMN     "logistics" TEXT NOT NULL DEFAULT 'Arranged by buyer';
-
--- AlterTable
-ALTER TABLE "Notification" ADD COLUMN     "status" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20241212034515_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `buyerId` on the `requiredCrops` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "requiredCrops" DROP COLUMN "buyerId",
-ADD COLUMN     "userId" TEXT[];
